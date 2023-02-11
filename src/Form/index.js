722e90c17c86c2f25646399f978144b9137ce939
Katarzyna Migacz <k.migacz94@gmail.com>
2023-02-11 16:48:37 +0100

    code formated

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,9 +2,9 @@ import "./style.css";
 
 const Form = () => (
     <form className="form">
-        <input className="form__input" autoFocus placeholder="Co jest do zrobienia?"/>
+        <input className="form__input" autoFocus placeholder="Co jest do zrobienia?" />
         <button className="form__button">Dodaj zadanie</button>
-     </form>
+    </form>
 );
 
 export default Form;
